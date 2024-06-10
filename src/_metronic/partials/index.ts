e6828eada54137727867aea21db9d29cddad2ac3
kofi-k <kofivincent026@gmail.com>
2024-06-10 08:29:10 +0000

    removed unused components

--- src/_metronic/partials/index.ts
+++ src/_metronic/partials/index.ts
@@ -1,13 +1,3 @@
-export * from './layout/quick-links/QuicLinks'
-export * from './layout/search/Search'
-export * from './layout/SearchModal'
-export * from './layout/InboxCompose'
-export * from './layout/RightToolbar'
-export * from './layout/header-menus/HeaderNotificationsMenu'
-export * from './layout/drawer-messenger/DrawerMessenger'
-export * from './layout/activity-drawer/ActivityDrawer'
-export * from './layout/user-menu/UserMenu'
-export * from './layout/create-app/CreateApp'
 export * from './layout/theme-mode/ThemeModeSwitcher'
 
 // dropdpwns
@@ -22,10 +12,4 @@ export * from './content/code-highlight/CodeBlock'
 export * from './content/portal/Portal'
 
 // modals
-export * from './modals/invite-users/InviteUsers'
-export * from './modals/upgrade-plan/UpgradePlan'
 export * from './modals/create-app-stepper/CreateAppModal'
-export * from './modals/select-location/SelectLocationModal'
-
-// chat
-export * from './chat/ChatInner'
