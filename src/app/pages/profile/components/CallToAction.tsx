4120463856461c3f22bc38f893f4571f8e49422d
kofi-k <kofivincent026@gmail.com>
2024-06-14 08:47:31 +0000

    framer animations added

--- src/app/pages/profile/components/CallToAction.tsx
+++ src/app/pages/profile/components/CallToAction.tsx
@@ -4,16 +4,15 @@ import {KTIcon} from "../../../../_metronic/helpers";
 export const CallToAction = () => {
     return (
         <>
-            <div className='card mb-5 mb-xl-8 bg-primary  my-20 kofi-masked-cta kofi-masked-block ' style={
-                {
-                    backgroundImage: 'linear-gradient(135deg, #006AE6, #2A6DF5)',
-                }
-
-            }>
+            <div className='card mb-5 mb-xl-8 bg-primary  my-20 kofi-masked-cta kofi-masked-block '
+                 style={{
+                     backgroundImage: 'linear-gradient(135deg, #006AE6, #2A6DF5)',
+                 }}
+            >
                 <div className='card-body my-10'>
                     <div className='d-flex flex-center flex-column flex-column-fluid mx-15'>
                         <h1 className='text-start fs-4hx text-white fw-bolder mb-5 w-100'>
-                            Ready to experience the <br/><span
+                            Ready to experience my <br/><span
                             className={'text-decoration-underline fst-italic'}>magic?</span>
                         </h1>
 
--- src/app/pages/profile/components/FramerReveal.tsx
+++ src/app/pages/profile/components/FramerReveal.tsx
@@ -3,29 +3,29 @@ import {motion, useAnimation, useInView} from "framer-motion";
 
 type Props = {
     children: React.ReactNode;
-    width: "auto" | "100";
     className?: string;
 }
 
 export const FramerReveal = ({
                                  children,
-                                 width = "auto",
                                  className = "",
                              }: Props) => {
     const ref = React.useRef<HTMLDivElement | null>(null);
     const isInView = useInView(ref, {once: true});
     const mainControls = useAnimation()
+    const slideControls = useAnimation()
 
     useEffect(() => {
         if (isInView) {
             mainControls.start('visible').then(
                 () => mainControls.set('hidden')
             )
+            slideControls.start('visible').then()
         }
-    }, [isInView, mainControls]);
+    }, [isInView, mainControls, slideControls]);
 
     return (
-        <div ref={ref} className={`${className} w-${width} overflow-hidden position-relative`}>
+        <div ref={ref} className={`${className}  overflow-hidden position-relative`}>
             <motion.div
                 variants={
                     {
@@ -38,6 +38,23 @@ export const FramerReveal = ({
                 transition={{duration: 0.5, ease: "easeOut", delay: 0.25}}
             >
                 {children}
+            </motion.div>
+            <motion.div
+                variants={{
+                    hidden: {left: 0},
+                    visible: {left: '100%'}
+                }}
+                initial={'hidden'}
+                animate={slideControls}
+                transition={{duration: 0.5, ease: "easeIn",}}
+                className={'position-absolute bg-primary z-index-20'}
+                style={{
+                    top: 4,
+                    bottom: 4,
+                    left: 0,
+                    right: 0,
+                }}
+            >
             </motion.div>
 
         </div>
