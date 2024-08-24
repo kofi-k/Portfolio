b83c21473c7a4e82880bab281a4c007f39a5face
Kofi K <kofivincent026@gmail.com>
2024-08-24 18:59:13 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/",
+    base: "https://kofi-k.github.io/",
     build: {
         chunkSizeWarningLimit: 3000,
     },
